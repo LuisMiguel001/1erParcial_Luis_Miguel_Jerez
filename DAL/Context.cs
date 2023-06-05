dedcfa5062118310e4dea5e84406e41d2e92724e
Luis Miguel Jerez Cáceres <luis_jerez1@ucne.edu.do>
2023-06-05 18:39:34 -0400

    Agregando el contextot.

---
+++ DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using _1erParcial_Luis_Miguel_Jerez.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _1erParcial_Luis_Miguel_Jerez.DAL
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> Options)
+            :base(Options) { }
+
+        public DbSet<Ingresos> Ingresos { get; set; } 
+    }
+}
---
+++ Models/Ingresos.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _1erParcial_Luis_Miguel_Jerez.Models
+{
+    public class Ingresos
+    {
+        [Key]
+
+        public int IngresosId { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public string Concepto { get; set; }
+
+        public Decimal Monto { get; set; }
+    }
+}
