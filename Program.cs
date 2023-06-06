41e6aed76a34740f916c9c9b96e06708ed4ccda2
Luis Miguel Jerez Cáceres <luis_jerez1@ucne.edu.do>
2023-06-05 20:33:20 -0400

    Finalizando examen.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
