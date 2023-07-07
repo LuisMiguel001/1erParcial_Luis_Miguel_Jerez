61f40b811b088ec2c8f9cfdde9d8198dfb3e949e
Luis Miguel Jerez Cáceres <luis_jerez1@ucne.edu.do>
2023-07-07 12:58:02 -0400

    Agregando el detalle al primer parcial.

---
+++ Models/Detalle.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _1erParcial_Luis_Miguel_Jerez.Models
+{
+    public class Detalle
+    {
+        [Key]
+
+        [Required (ErrorMessage = "El Campo Emisor es obligatorio")]
+        public string? Emisor { get; set; }
+
+        [Required(ErrorMessage = "El Campo Emisor es obligatorio")]
+        public string? Mensaje { get; set; }
+    }
+}
